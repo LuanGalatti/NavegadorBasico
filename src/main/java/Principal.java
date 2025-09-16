cb3325aede119c3e931c5974f13ced1e73866554
LuanGalatti <luan.galatti@gmail.com>
2025-09-16 10:39:02 -0300

    Criacao do projeto

---
+++ src/main/java/Principal.java
@@ -0,0 +1,13 @@
+
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Janela janela = new Janela();
+        janela.setVisible(true);
+       
+    }
+}
